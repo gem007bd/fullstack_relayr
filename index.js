d6eb0f6fa46a5368c3cd4275b0c1f28ef7115ea4
md Aminul hoque <gem007bd@yahoo.com>
2019-09-04 23:23:57 +0200

    dont get it

--- index.js
+++ index.js
@@ -1,19 +1,19 @@
-'use strict'
+"use strict";
 
 // your solution
-class Finder{
+class Finder {
   // write the code to initialize your class here
   // the input is and array of string ex. ["asd", "wer"]
   constructor(arr) {
-
+    for (let i = 0; i < arr.lenght; i++) {
+      console.log(arr);
+    }
+    return key;
   }
 
   // write you code to get subset of initial array with given key
   // key is always string ex. "asd"
-  find(key) {
-
-  }
+  find(key) {}
 }
 
-
-module.exports = Finder
+module.exports = Finder;
